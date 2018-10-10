c815bc0db6d64c330050d5762fc77a783e201308
Kondoremb <30407873+darioemb@users.noreply.github.com>
2018-10-10 10:07:14 +0200

    Update slow.cpp

--- luci/solutions/slow.cpp
+++ luci/solutions/slow.cpp
@@ -1,31 +1,4 @@
 
-
-#include <iostream>
-#include <list>
-#include <stack>
-#include <vector>
-#include <bitset>
-#include <cstdio>
-#include <sstream>
-#include <climits>
-#include <algorithm>
-#include <queue>
-#include <set>
-#include <string.h>
-
-
-using namespace std;
-
-
-int spegni(int N, int M, int K)
-{
-
-
-    
-        int accese = 0;
-        int cnt = 1;
-
-
 #include <iostream>
 #include <list>
 #include <stack>
